577d8d8010db959bc4b0932f6cf0a9da0e462119
Usman Aslam Malik <usman.maliknu13@gmail.com>
2022-02-16 19:30:36 +0000

    changes

---
+++ src/widgets/Menu/theme.ts
@@ -0,0 +1,12 @@
+import { darkColors, lightColors } from "../../theme/colors";
+import { NavTheme } from "./types";
+
+export const light: NavTheme = {
+  background: "#201854",
+  hover: "#201854",
+};
+
+export const dark: NavTheme = {
+  background: "#201854",
+  hover: "#201854",
+};
